58f167dac06b601c7c56630b5093c176f0579e7e
Gabriel Vieira <gabriel@evolucaoagil.com.br>
2014-07-20 09:14:28 -0300

    Ajustes CSS Sidebar de notícias buscando automaticamente, mas ainda falta estar a mudança de linguagem Home busca noticias e patrocinadores automaticamente

--- wp-content/themes/agile-brazil-2014/functions.php
+++ wp-content/themes/agile-brazil-2014/functions.php
@@ -18,17 +18,13 @@ function ab2014_register_menus() {
 add_action( 'init', 'ab2014_register_menus' );
 
 
-function ab2014_widgets_init() {
-
-    register_sidebar( array(
-        'name' => 'Home right sidebar',
-        'id' => 'home_right_1',
-        'before_widget' => '<div>',
-        'after_widget' => '</div>',
-        'before_title' => '<h2 class="rounded">',
-        'after_title' => '</h2>',
-    ) );
+function new_excerpt_more( $more ) {
+    return ' <a href="'. get_permalink( get_the_ID() ) .'">[Leia mais...]</a>';
 }
-add_action( 'widgets_init', 'ab2014_widgets_init' );
+add_filter('excerpt_more', 'new_excerpt_more');
 
+function custom_excerpt_length( $length ) {
+    return 20;
+}
+add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
 ?>
--- wp-content/themes/agile-brazil-2014/index.php
+++ wp-content/themes/agile-brazil-2014/index.php
@@ -1,7 +1,11 @@
 <?php
 get_header();
 if (is_front_page()){
-    $postid = 170;
+    if(pll_current_language() == 'pt'){
+        $postid = 99;
+    }else{
+        $postid = 170;
+    }
 }else{
     $postid = get_the_ID();
 }
@@ -14,6 +18,8 @@ $content = apply_filters('the_content', $post->post_content);
 echo $content;
 if (!is_front_page()){
     get_header('inside-end');
+}else{
+    get_header('home');
 }
 get_footer();
 ?>
